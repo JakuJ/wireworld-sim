d9e85da93bcba0e8a8014daaac0ff1ffc3c3e66a
Jakub Janaszkiewicz <janaszkiewicz.jakub@gmail.com>
2018-04-10 00:58:13 +0200

    Wireworld grids editor added
    Added an option to draw your own grid files instead of having them done in notepad.

--- app/Main.hs
+++ app/Main.hs
@@ -1,16 +1,18 @@
 module Main where
 
 import System.IO (stdin, hSetBuffering, hSetEcho, BufferMode (LineBuffering, NoBuffering))
-import System.Console.ANSI (hideCursor, showCursor, clearScreen, setCursorPosition, clearFromCursorToLineEnd)
+import System.Console.ANSI -- too many functions to list them all
 import GHC.Conc (ThreadStatus, ThreadId, threadStatus, threadDelay, killThread)
 import Control.Concurrent (MVar, newMVar, swapMVar, modifyMVar_, forkFinally)
-import Control.Monad (forever)
+import Control.Monad (forever, unless)
 import Control.Monad.Loops (iterateWhile)
 import Data.Tuple (swap)
+import Data.List (find, delete)
 import Data.List.Split (chunksOf)
-import Data.Maybe (fromMaybe)
+import Data.Maybe (fromMaybe, fromJust, isJust, isNothing)
+import Data.Char (digitToInt)
 
-data State = Empty | Wire | Head | Tail deriving (Show, Eq)
+data State = Empty | Wire | Head | Tail deriving (Eq, Enum)
 
 data Cell = Cell {
     state :: State,
@@ -28,6 +30,9 @@ data Automaton = Automaton {
     grid :: Grid,
     generation :: Int
 }
+
+instance Show State where -- overloaded Show instance for type State
+    show state = fromJust $ lookup (stateToChar state) colors
 
 instance Show Grid where -- overloaded Show instance for type Grid
     show grid = concatMap (\c -> fromMaybe "\n" (lookup c colors)) (gridToString grid)
@@ -91,10 +96,76 @@ calculatePeriod automat = recursiveCheck (grid automat) []
             | grid `elem` gs = Just $ (+) 1 $ length $ takeWhile (/= grid) $ tail $ dropWhile (/= grid) gs
             | otherwise = recursiveCheck (nextGeneration grid) $ gs ++ [grid]
 
-main :: IO () -- main program function
-main = do
+getChar' :: IO Char -- getChar without echo
+getChar' = do 
+    hSetEcho stdin False
+    input <- getChar
+    hSetEcho stdin True
+    return input
+
+fillGrid :: Grid -> Grid -- fills Grid which only has non-Empty cells to m x n size with Empty cells
+fillGrid (Grid cells ms ns) = Grid (map (\(x, y) -> (fromMaybe (Cell Empty x y) (found x y))) (cart [0 .. ms - 1] [0 .. ns - 1])) ms ns
+    where
+        found x y = find (\(Cell s m n) -> m == x && n == y) cells
+        cart :: [a] -> [b] -> [(a, b)] -- Cartesian product of two vectors
+        cart xs ys = [(x, y) | x <- xs, y <- ys]
+
+createNewAutomaton :: IO ()
+createNewAutomaton = do
+    putStrLn "Enter board size (m n): "
+    dims <- map (\x -> read x :: Int) . words <$> getLine
+    let [m, n] = dims
+    drawBorder m n
+    putStrLn "WSAD - move, 0 - empty, 1 - wire, 2 - e. head, 3 - e. tail, 9 - finish"
+    -- * main logic
+    setCursorPosition 1 1
+    newGrid <- fillGrid <$> modifyGrid (Grid [] m n)
+    putStrLn "Enter new Automaton's save path (empty for no save): "
+    input <- getLine
+    unless (null input) $ saveGrid newGrid input
+    putStrLn "Creating new automaton successfull!"
+    where
+        drawBorder :: Int -> Int -> IO () -- draws a border of size m x n
+        drawBorder m n = do
+            clearScreen
+            setCursorPosition 0 0
+            putStrLn $ replicate (n + 2) '-'
+            mapM_ (\_ -> putStr "|" >> cursorForward n >> putStrLn "|") [1 .. m]
+            putStrLn $ replicate (n + 2) '-'
+        movements :: [(Char, IO ())] -- movement functions lookup table
+        movements = [('w', cursorUp 1), ('s', cursorDown 1), ('a', cursorBackward 1), ('d', cursorForward 1)]
+        move :: Char -> Int -> Int -> IO () -- move cursor in terminal using wsad, can't go over the border
+        move c m n = do
+            pos <- fromMaybe (0, 0) <$> getCursorPosition
+            if (fst pos == 2 && c == 'w') || (fst pos == m + 1 && c == 's') || (snd pos == 2 && c == 'a') || (snd pos == n + 1 && c == 'd') 
+                then putStr ""
+            else fromJust $ lookup c movements
+        modifyGrid :: Grid -> IO Grid -- create new Grid by adding cells using accumulative recursion
+        modifyGrid grid = do
+            input <- getChar'
+            if input `elem` "wsad" then move input m n >> modifyGrid grid
+            else case input of
+                x | x `elem` "0123" -> do
+                    pos <- fromJust <$> getCursorPosition
+                    let chosenState = stateMap !! digitToInt x
+                    (putStr . show) chosenState
+                    cursorBackward 1
+                    modifyGrid (Grid (cells grid ++ [uncurry (Cell chosenState) ((\(x,y) -> (x-2, y-2)) pos)]) m n)
+                '9' -> do
+                    setCursorPosition (m + 3)  0
+                    if null (cells grid) 
+                        then return $ Grid [] 0 0
+                    else return $ Grid (foldl (\c1 c2 -> if row (last c1) == row c2 && column (last c1) == column c2 then init c1 ++ [c2] else c1 ++ [c2]) [head (cells grid)] (tail (cells grid))) m n
+                _ -> modifyGrid grid
+            where
+                m = rows grid
+                n = columns grid
+                stateMap = enumFrom (toEnum 0) :: [State]
+
+simulateAutomaton :: IO () -- simulate a Wireworld automaton
+simulateAutomaton = do
     -- * load Automaton file
-    putStrLn "Enter Automaton save file path: "
+    putStr "Enter Automaton save file path: "
     automatSav <- fmap (`Automaton` 0) $ loadGrid =<< getLine
     automatM <- newMVar automatSav
     -- * set console parameters
@@ -104,7 +175,7 @@ main = do
     hSetEcho stdin False
     -- * spawn animation thread
     animationThread <- startAnimationThread automatM
-    -- * interface (main thread)
+    -- * animation interface (main thread)
     iterateWhile (==True) $ do -- TODO: Menu
         input <- getChar
         case input of
@@ -118,12 +189,12 @@ main = do
     hSetBuffering stdin LineBuffering
     hSetEcho stdin True
     -- * print Automaton's period
-    let period = fromMaybe (-1) $ calculatePeriod automatSav
-    if period < 0 then putStrLn "No period" else putStrLn $ "Period of " ++ show period
+    let period = calculatePeriod automatSav
+    if isNothing period then putStrLn "No period" else putStrLn $ "Period of " ++ show (fromJust period)
     -- * finish execution
     killThread animationThread -- ? unnecessary (main thread kills all children upon termination), but good practice
     threadDelay 1000 -- TODO: atomic putStrLn
-    putStrLn "Main thread terminated" 
+    putStrLn "Simulation function ended"
         where
             delay = 500000 :: Int -- simulation's time step 
             startAnimationThread :: MVar Automaton -> IO ThreadId
@@ -135,3 +206,18 @@ main = do
                     putStrLn  ("Generation " ++ (show . generation) automat1) 
                     return $ Automaton ((nextGeneration . grid) automat1) (generation automat1 + 1) 
                 threadDelay delay
+
+main :: IO () -- program entry point
+main = do
+    -- * main menu
+    putStrLn "Menu:"
+    putStrLn "1 - Load Grid from file"
+    putStrLn "2 - Create new Grid"
+    putStrLn "Other - Quit"
+    putStr "Choice: "
+    choice <- getChar
+    putStrLn ""
+    case choice of
+        '1' -> simulateAutomaton
+        '2' -> createNewAutomaton
+        _ -> putStrLn "Main thread terminated"
